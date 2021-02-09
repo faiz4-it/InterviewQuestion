71c8b9b9cc6d243b84107adcb682daaa789896e5
fasihullah <faiz24.it@gmail.com>
2021-02-09 12:46:10 +0530

    "Java 8 Feature"

---
+++ Java8Practice/src/main/java/com/faiz/Java8Practice/App.java
@@ -0,0 +1,13 @@
+package com.faiz.Java8Practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
